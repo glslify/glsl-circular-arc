1eb488d11a95d5e931743a23c1c8ff6379c3439b
substack <substack@9nTgtYmvW4HID6ayt6Icwc8WZxdifx5SlSKKIX/X/1g=.ed25519>
2017-08-14 00:20:43 -1000

    basically pretty sweet

--- example/main.js
+++ example/main.js
@@ -6,13 +6,14 @@ var menu = regl({
   frag: `
     precision highp float;
     varying vec2 vpos;
-    uniform vec2 size;
+    uniform vec2 size, radius;
     void main () {
       float dz = 10.0/min(size.x,size.y);
       float r = length(vpos);
-      float d = smoothstep(1.0-dz,1.0,r)
-        + smoothstep(0.5+dz,0.5,r);
-      gl_FragColor = vec4(vec3(1.0-d),(1.0-d)*0.5+0.5);
+      if (r < radius.x || r > radius.y) discard;
+      float d = smoothstep(radius.y-dz,radius.y,r)
+        + smoothstep(radius.x+dz,radius.x,r);
+      gl_FragColor = vec4(vec3(1.0-d),(1.0-d));
     }
   `,
   vert: `
@@ -25,10 +26,8 @@ var menu = regl({
         min(size.x,size.y)/size.x,
         min(size.x,size.y)/size.y
       );
-      float t = position.x;
-      float th = min(theta.y,max(theta.x,t));
-      //vpos = min(vec2(1),max(vec2(-1),vec2(cos(th),sin(th))*2.0))
-      //  * position.y;
+      float tau = ${Math.PI*2};
+      float th = min(theta.y,max(theta.x,position.x));
       vpos = vec2(cos(th),sin(th)) * 2.0 * position.y;
       float x = max(abs(vpos.x),abs(vpos.y))-0.001;
       vpos.x = vpos.x / x;
@@ -46,7 +45,8 @@ var menu = regl({
       size[1] = context.viewportHeight
       return size
     },
-    theta: regl.prop('theta')
+    theta: regl.prop('theta'),
+    radius: regl.prop('radius')
   },
   attributes: {
     position: mesh.positions
@@ -61,5 +61,10 @@ regl.frame(frame)
 function frame (context) {
   //regl.poll()
   regl.clear({ color: [0,0,0,1], depth: true })
-  menu({ theta: [0.2,0.2+(Math.sin(context.time)*0.5+0.5)*4] })
+  var t = context.time
+  menu([
+    { theta: [0,(Math.sin(t)*0.5+0.5)*2*Math.PI], radius: [0.25,0.5] },
+    { theta: [1*(Math.sin(t)*0.5+0.5)*4,(Math.sin(t)*0.5+0.5)*2*Math.PI], radius: [0.5,0.75] },
+    { theta: [2,(Math.sin(t)*0.5+0.5)*2*Math.PI], radius: [0.75,1] },
+  ])
 }
